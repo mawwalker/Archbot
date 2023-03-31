bdaafd85a3555742d2a4218b9e5877fd6cbc9cfd
Dengshima <dengshima@gmail.com>
2023-03-31 12:20:50 +0000

    root route

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ from flask import Flask
 from flask import request
 from flask import Response
 from tgbot import TG
+import json
 
 app = Flask(__name__)
+with open('config/config.json', 'r') as f:
+    data = json.loads(f.read())
+config = data
 
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route(f"/{config['access_token']}", methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         msg = request.get_json()
